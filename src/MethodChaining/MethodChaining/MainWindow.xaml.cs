3103f999d48cfd5429be2b2a95ecaf251d3936d6
nuhabo <nuhabo03@gmail.com>
2021-12-04 18:19:37 +0900

    Create MethodChaining

---
+++ src/MethodChaining/MethodChaining/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MethodChaining
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SampleMethod();
+        }
+
+        private void SampleMethod()
+        {
+
+            int[] numbers = { 2, 3, 5, 6 };
+
+            var getValue = numbers.Select(x => x * 2)
+                                  .Where(x => x != 4)
+                                  .OrderBy(x => x);
+
+
+        }
+    }
+}
