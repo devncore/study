4762154174a18dee5ee6b1352c98ba5070304785
james.lee <james.lee@devncore.org>
2022-05-13 07:45:13 +0900

    Update study

---
+++ src/FindLocationApp/FindLocationApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+bool result = true;
+
+while (result)
+{
+    Console.Write("Input process name: ");
+    string? name = Console.ReadLine();
+
+    if (!string.IsNullOrWhiteSpace(name))
+    {
+        var apps = Process.GetProcesses().ToList();
+        var find = apps.Where(X => X.ProcessName.ToUpper().Contains(name.ToUpper()));
+
+        Console.WriteLine($"Found apps: {find.Count()}");
+        foreach (var item in find)
+        {
+            Console.WriteLine("Name: " + item.ProcessName);
+            Console.WriteLine("Path: " + item.MainModule.FileName);
+        }
+    }
+}
