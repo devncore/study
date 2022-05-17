340f76483929c1192fda5edfe66fc1e37007ccd4
james.lee <james.lee@devncore.org>
2022-05-17 21:32:00 +0900

    Update source

--- src/ListViewExample1/ListViewExample1/MainWindow.xaml.cs
+++ src/ListViewExample1/ListViewExample1/MainWindow.xaml.cs
@@ -18,13 +18,13 @@ namespace ListViewExample1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+public partial class MainWindow : Window
+{
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
+        InitializeComponent();
 
-            DataContext = new MainViewModel();
-        }
+        DataContext = new MainViewModel();
     }
+}
 }
